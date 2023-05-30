413bd2dce44c8473efe129a74cfd5c21f30d89ad
Stefano Salonen <[stasal@utu.fi]>
2023-05-30 13:35:55 +0300

    Added CSS for UI improvement

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom'
+import "./App.css"
 
 import Journeys from './components/Journeys';
 import Stations from './components/Stations';
 import Station from './components/Station';
+
 
 function App() {
 
@@ -12,17 +14,19 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
-          <h1>Helsinki City Bike App </h1>
+          <Link to="/" style={{ color: 'inherit', textDecoration: 'inherit'}}>
+            <h1>Helsinki City Bike App </h1>
+          </Link>
         </header>
           <Routes>
             <Route path="/journeys" element={<Journeys />} />
             <Route path="/stations/:id" element={<Station />} />
             <Route path="/stations" element={<Stations />} />
             <Route path="/" element={
-              <div>
+              <div className="center-div">
                 <h2>Welcome to Helsinki City Bike App</h2>
-                <p><Link to="/journeys">View Journeys</Link></p>
-                <p><Link to="/stations">View Stations</Link></p>
+                <p><Link to="/journeys" className="main-button">View Journeys</Link></p>
+                <p><Link to="/stations" className="main-button">View Stations</Link></p>
               </div>
             }/>
           </Routes>
--- frontend/src/components/Journeys.js
+++ frontend/src/components/Journeys.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react'
 function Journeys() {
     const [journeys, setJourneys ] = useState([])
     const [page, setPage] = useState(1)
-    const size = 100
+    const size = 50
 
     useEffect(() => {
         fetch(`http://localhost:3001/api/journeys?page=${page}&size=${size}`)
@@ -11,6 +11,10 @@ function Journeys() {
           .then(data => setJourneys(data))
     
       }, [page])
+      
+    const convertDistanceToKm = (meters) => {
+        return (meters / 1000).toFixed(2);
+    } 
 
     const convertDurationToMinSec = (sec) => {
       const minutes = Math.floor(sec / 60)
@@ -25,13 +29,15 @@ function Journeys() {
           <ul>
             {journeys.map(journey => (
               <li key={journey.id}>
-                Departure: {journey.departure_station_name} - Return: {journey.return_station_name} - Distance: {journey.covered_distance_m} m - Duration: {convertDurationToMinSec(journey.duration_sec)}
+                <p><b>Departure: </b> {journey.departure_station_name}</p>
+                <p><b>Return: </b>{journey.return_station_name}</p>
+                <p><b>Distance: </b>{convertDistanceToKm(journey.covered_distance_m)} km - <b>Duration: </b>{convertDurationToMinSec(journey.duration_sec)}</p>
               </li>
             ))}
           </ul>
 
-          <button onClick={() => setPage(page => Math.max(page - 1, 1))}>Perviouos Page</button>
-          <button onClick={() => setPage(page => page + 1)}>Next Page</button>
+          <button className="pagination-button" onClick={() => setPage(page => Math.max(page - 1, 1))}>Pervious</button>
+          <button className="pagination-button" onClick={() => setPage(page => page + 1)}>Next</button>
         </div>
       )
 }
